5ff2ba928756b2f38618f11d62f7067c308350a5
Angelina <an.mischenko2002@gmail.com>
2023-10-10 15:11:35 +0300

    Created Repository

---
+++ app/src/main/java/com/example/shoppinglist/data/NoteRepoImpl.kt
@@ -0,0 +1,21 @@
+package com.example.shoppinglist.data
+
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepoImpl(private val dao: NoteDao) : NoteRepository {
+    override suspend fun insertItem(item: NoteItem) {
+        dao.insertItem(item)
+    }
+
+    override suspend fun deleteItem(item: NoteItem) {
+        dao.deleteItem(item)
+    }
+
+    override fun getAllItems(): Flow<List<NoteItem>> {
+        return dao.getAllItems()
+    }
+
+    override suspend fun getNoteItemById(id: Int): NoteItem {
+        return dao.getNoteItemById(id)
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/data/ShoppingListRepoImpl.kt
@@ -0,0 +1,17 @@
+package com.example.shoppinglist.data
+
+import kotlinx.coroutines.flow.Flow
+
+class ShoppingListRepoImpl(private val dao: ShoppingListDao) : ShoppingListRepository {
+    override suspend fun insertItem(item: ShoppingListItem) {
+        dao.insertItem(item)
+    }
+
+    override suspend fun deleteItem(item: ShoppingListItem) {
+        dao.deleteItem(item)
+    }
+
+    override fun getAllItems(): Flow<List<ShoppingListItem>> {
+        return dao.getAllItems()
+    }
+}
