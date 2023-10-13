377ac9a1c2f02b1d50ed9efc6a1ef2ca85b2e029
Angelina <an.mischenko2002@gmail.com>
2023-10-13 11:31:46 +0300

    Created UI shopping list item

---
+++ app/src/main/java/com/example/shoppinglist/shopping_list_screen/UiShoppingListItem.kt
@@ -0,0 +1,143 @@
+package com.example.shoppinglist.shopping_list_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.shoppinglist.ui.theme.DarkText
+import com.example.shoppinglist.ui.theme.LightText
+import com.example.shoppinglist.R
+import com.example.shoppinglist.ui.theme.Green
+import com.example.shoppinglist.ui.theme.Red
+
+@Preview(showBackground = true)
+@Composable
+fun UiShoppingListItem() {
+    ConstraintLayout(
+        modifier = Modifier.padding(
+            start = 5.dp,
+            top = 15.dp,
+            end = 5.dp
+        )
+    ) {
+        val (card, deleteButton, editButton, counter) = createRefs()
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(card) {
+                    // Куда прикрепляем
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
+                Text(
+                    text = "list1",
+                    style = TextStyle(
+                        color = DarkText,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                )
+                Text(
+                    text = "12/12/2023 13:00",
+                    style = TextStyle(
+                        color = LightText,
+                        fontSize = 12.sp
+                    )
+                )
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 5.dp)
+                )
+            }
+        }
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .constrainAs(deleteButton) {
+                    top.linkTo(card.top)
+                    bottom.linkTo(card.top)
+                    end.linkTo(card.end)
+                }
+                .padding(end = 10.dp)
+                .size(30.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.delete_icon),
+                contentDescription = "Delete",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(Red)
+                    .padding(5.dp),
+                tint = Color.White
+            )
+        }
+
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .constrainAs(editButton) {
+                    top.linkTo(card.top)
+                    bottom.linkTo(card.top)
+                    end.linkTo(deleteButton.start)
+                }
+                .padding(end = 5.dp)
+                .size(30.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.edit_icon),
+                contentDescription = "Edit",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(Green)
+                    .padding(5.dp),
+                tint = Color.White
+            )
+        }
+
+        Card(shape = RoundedCornerShape(5.dp),
+            modifier = Modifier
+                .constrainAs(counter) {
+                    top.linkTo(card.top)
+                    bottom.linkTo(card.top)
+                    end.linkTo(editButton.start)
+                }
+                .padding(end = 5.dp)) {
+            Text(
+                text = "15/5",
+                modifier = Modifier
+                    .background(Red)
+                    .padding(
+                    top = 5.dp,
+                    bottom = 5.dp,
+                    start = 5.dp,
+                    end = 5.dp
+                ),
+                color = Color.White
+            )
+
+        }
+    }
+}
--- app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -2,9 +2,10 @@ package com.example.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val Green = Color(0x9067D569)
+val Red = Color(0x94C54A4E)
 val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkText = Color(0xFF1A1A1A)
+val LightText = Color(0xFF5C5C5C)
 val BlueLight = Color(0xFF03A9F4)
 val GreyLight = Color(0xFF999999)
